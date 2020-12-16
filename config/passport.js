29c131858356ae306965ee35fc69f1ff41b7a078
ajayrmk <ajayram192@gmail.com>
2020-12-17 03:20:46 +0400

    first commit

---
+++ config/passport.js
@@ -0,0 +1,62 @@
+// config/passport.js
+
+// load all the things we need
+var LocalStrategy = require('passport-local').Strategy;
+
+// Generate Hash
+var bcrypt = require('bcrypt-nodejs');
+
+// database module
+var database = require('../config/database');
+var RunQuery = database.RunQuery;
+
+// expose this function to our app using module.exports
+module.exports = function (passport) {
+
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function (user, done) {
+        done(null, user.UserID);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function (userId, done) {
+        var sqlStr = '\
+            SELECT *\
+            FROM Users\
+            where UserID = \'' + userId + '\'';
+        RunQuery(sqlStr, function (rows) {
+            done(null, rows[0]);
+        });
+    });
+
+
+    passport.use('sign-in', new LocalStrategy({
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true // allows to pass back the entire request to the callback
+        },
+        function (req, username, password, done) { // callback with username and password from form
+            // check to see if the user exists or not
+            var sqlStr = 'SELECT * FROM Users WHERE Username = \'' + username + '\'';
+            RunQuery(sqlStr, function (rows) {
+                // if no user is found, return the message
+                if (rows.length < 1)
+                    return done(null, false, req.flash('signInError', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+                // if the user is found but the password is wrong
+                if (!bcrypt.compareSync(password, rows[0].Password))
+                    return done(null, false, req.flash('signInError', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+                // all is well, return successful user
+                return done(null, rows[0]);
+            });
+
+        })
+    );
+};
